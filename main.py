15e8f281b1f5dbc18c1d963ad1640bbf9abf84c8
Chase Ottimo <chaseottimo@gmail.com>
2026-09-01 07:35:20 -0400

    main.py conditionals on status codes

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+
+username = input("Input a Github username:")
+
+profile_response = requests.get(f"https://api.github.com/users/{username}")
+
+profile_data = profile_response.json()
+
+if profile_response.status_code == 200:
+    print(f"Login: {profile_data['login']}")
+    print(f"Name: {profile_data['name']}")
+    print(f"Public Repos: {profile_data['public_repos']}")
+    print(f"Followers: {profile_data['followers']}")
+    print(f"URL: {profile_data['html_url']}")
+elif profile_response.status_code == 404:
+    print("Github user not found.")
